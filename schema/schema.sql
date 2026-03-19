ac2924f0842b51203b1cb1f1f6dfcc0d77d3c42c
jraldGITHUB <jraldybaco@gmail.com>
2026-03-19 21:50:55 +0800

    edit login

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,7 +1,8 @@
 CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
+id INT AUTO_INCREMENT PRIMARY KEY,
+username VARCHAR(50) UNIQUE NOT NULL,
+password VARCHAR(255) NOT NULL,
+role ENUM('admin','manager','user')
 );
 
 CREATE TABLE rooms (
